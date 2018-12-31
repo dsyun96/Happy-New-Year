bbf8f404fe63fb732e92013c697043c2fcc1ab34
Piorosen <aoikazto@gmail.com>
2019-01-01 00:26:28 +0900

    C#

---
+++ HNY.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Happy_New_Year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy-New-Year");
+        }
+    }
+}
